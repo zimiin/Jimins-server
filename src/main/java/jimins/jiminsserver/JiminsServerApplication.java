75b4ec11c8130dccc8c51f9ef31c86f9b055a28d
Jimin Shin <45932570+zimiin@users.noreply.github.com>
2022-09-10 22:55:43 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/jimins/jiminsserver/JiminsServerApplication.java
@@ -0,0 +1,13 @@
+package jimins.jiminsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiminsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiminsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jimins/jiminsserver/JiminsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jimins.jiminsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiminsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
