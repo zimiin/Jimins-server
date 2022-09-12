6b04428550182f0617febd36aac0df32ef2cb926
Jimin Shin <45932570+zimiin@users.noreply.github.com>
2022-09-12 16:34:47 +0900

    @Entity 추가 및 @Id 명시

--- src/main/java/jimins/jiminsserver/domain/Word.java
+++ src/main/java/jimins/jiminsserver/domain/Word.java
@@ -1,14 +1,21 @@
 package jimins.jiminsserver.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Word {
 
-    private final String word;
-
-    public Word(String word) {
-        this.word = word;
-    }
+    @Id
+    private String word;
 
     public String getWord() {
         return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
     }
 }
