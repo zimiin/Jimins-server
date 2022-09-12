59caac036d070be80e1a9bfebedbe1defc450786
Jimin Shin <45932570+zimiin@users.noreply.github.com>
2022-09-12 16:48:37 +0900

    @Transactional 추가

--- src/test/java/jimins/jiminsserver/service/WordServiceTest.java
+++ src/test/java/jimins/jiminsserver/service/WordServiceTest.java
@@ -6,9 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 public class WordServiceTest {
 
     @Autowired
