2db80131a44f2c0466384b9d2c3d5e7ff4f57132
Jimin Shin <45932570+zimiin@users.noreply.github.com>
2022-09-12 16:48:50 +0900

    GET /word 구현

--- src/main/java/jimins/jiminsserver/controller/WordController.java
+++ src/main/java/jimins/jiminsserver/controller/WordController.java
@@ -1,19 +1,23 @@
 package jimins.jiminsserver.controller;
 
 import jimins.jiminsserver.domain.Word;
+import jimins.jiminsserver.service.WordService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class WordController {
 
+    private final WordService wordService;
+
+    public WordController(WordService wordService) {
+        this.wordService = wordService;
+    }
+
     @GetMapping("/word")
     public List<Word> words() {
-        List<Word> wordList = new ArrayList<>();
-        wordList.add(new Word("test"));
-        return wordList;
+        return wordService.findWords();
     }
 }
